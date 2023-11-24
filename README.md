4519443dd27896a0c6d5ba07d8e586dd462e9756
riskibowo <115862112+riskibowo@users.noreply.github.com>
2023-11-24 20:09:23 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PEMROGRAMAN-MOBILE-
+# AndroidST
+
+![Screenshot (246)](https://github.com/DimasF3009/AndroidST/assets/115356128/d3001b9a-1931-44bb-af9d-ed6f5a23ca59)
---
+++ app/src/main/java/com/example/latihanapp4/FiboActivity.java
@@ -0,0 +1,67 @@
+package com.example.latihanapp4;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FiboActivity extends AppCompatActivity {
+    private Context context;
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+
+    private int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            int fib1 = 0;
+            int fib2 = 1;
+            int result = 0;
+
+            for(int i = 2; i<= n; i++){
+                result= fib1+fib2;
+                fib1 = fib2;
+                fib2 =result;
+            }
+            return result;
+        }
+    }
+    public void showToast(View view){
+        String myString = "Hallo Toast";
+        Toast toast = Toast.makeText(getApplicationContext(), myString, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view){
+//        Hitung nilai saat ini
+        int fibonacciValue = fibonacci(mCount);
+
+//        Tambah nilai ke text
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(fibonacciValue));
+//            ubah warna text
+            if (fibonacciValue %2 == 0){
+                mShowCount.setTextColor(getResources().getColor(R.color.black));
+            }else{
+                mShowCount.setTextColor(getResources().getColor(R.color.blue));
+            }
+        }
+        mCount++;
+    }
+    public void resetCount(View view) {
+        mCount = 0;
+        mShowCount.setText("0");
+        mShowCount.setTextColor(getResources().getColor(R.color.black));
+    }
+}
